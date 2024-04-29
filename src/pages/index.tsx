05c30f8f48588b463b6aac7821ea392decbbc98e
Tony <tawn33y@gmail.com>
2024-04-29 00:38:42 -0400

    Add responsiveness; split into components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,17 @@
 'use client'
-import { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { FactoryData } from "../api/getFactoryData";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState<FactoryData | null>(null);
+interface ChartsCanvasProps {
+  data: FactoryData;
+}
 
-  useEffect(() => {
-    import('../api/getFactoryData').then(({ getFactoryData }) => {
-      getFactoryData()
-        .then((result) => setData(result))
-        .catch((error) => setError(error))
-        .finally(() => setIsLoading(false));
-    }).catch((error) => setError(error?.message ?? 'An error occurred'));
-  }, []);
-
-  if (isLoading) return (<p>Loading...</p>);
-  if (error) return (<p className="text-red-500">{error}</p>);
-  if (!data) return (<p>No data</p>);
-
-  return (
-    <div className="p-4">
-      <div className="w-full">
-      {/* <ResponsiveContainer width="100%" height="100%"> */}
-        <p>Food waste (Kg)</p>
+const ChartsCanvas: FC<ChartsCanvasProps> = ({ data }) => (
+  <div className="p-4">
+    <p>Food waste (Kg)</p>
+    <div className="w-full h-96">
+      <ResponsiveContainer width="100%" height="100%">
         <LineChart
           width={500}
           height={300}
@@ -49,8 +35,30 @@ export default function Home() {
           <Line type="monotone" dataKey="foodWaste" stroke="#8884d8" activeDot={{ r: 8 }} />
           <Line type="monotone" dataKey="waterConsumption" stroke="#82ca9d" activeDot={{ r: 8 }} />
         </LineChart>
-      {/* </ResponsiveContainer> */}
-      </div>
+      </ResponsiveContainer>
     </div>
+  </div>
+);
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState<FactoryData | null>(null);
+
+  useEffect(() => {
+    import('../api/getFactoryData').then(({ getFactoryData }) => {
+      getFactoryData()
+        .then((result) => setData(result))
+        .catch((error) => setError(error))
+        .finally(() => setIsLoading(false));
+    }).catch((error) => setError(error?.message ?? 'An error occurred'));
+  }, []);
+
+  if (isLoading) return (<p>Loading...</p>);
+  if (error) return (<p className="text-red-500">{error}</p>);
+  if (!data) return (<p>No data</p>);
+
+  return (
+    <ChartsCanvas data={data} />
   );
 }
