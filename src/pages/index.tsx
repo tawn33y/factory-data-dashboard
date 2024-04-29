60d08a594011cde1be28cf0b3e0b47231ffccf97
Tony <tawn33y@gmail.com>
2024-04-29 01:07:06 -0400

    Add separate charts +combined

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,95 @@
-'use client'
+'use client';
 import { FC, useEffect, useState } from "react";
 import { FactoryData } from "../api/getFactoryData";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+// TODO: add different colors
+// move to components/
+// loop over array->charts
+// add dropdown/toggle
+// add pie chart for combined
+
+interface LineChartCustomProps {
+  title: string;
+  data: Record<string, string | number>[];
+}
+
+const LineChartCustom: FC<LineChartCustomProps> = ({ title, data }) => {
+  const { name, ...dataPoints } = data[0];
+
+  return (
+    <div className="bg-white p-4 shadow-md rounded-lg">
+      <p className="font-bold mb-4">{title}</p>
+      <div className="w-96 h-96">
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 10,
+              right: 10,
+              left: -10,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            {Object.keys(dataPoints).map((dataPoint) => (
+              <Line
+                key={dataPoint}
+                type="monotone"
+                dataKey={dataPoint}
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+            ))}
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
 
 interface ChartsCanvasProps {
   data: FactoryData;
 }
 
 const ChartsCanvas: FC<ChartsCanvasProps> = ({ data }) => (
-  <div className="p-4">
-    <p>Food waste (Kg)</p>
-    <div className="w-full h-96">
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data.headers.map((header, idx) => ({
-            name: header,
-            foodWaste: data.rows['Food waste (Kg)'][idx],
-            waterConsumption: data.rows['Water consumption (m3)'][idx],
-          }))}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="foodWaste" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="waterConsumption" stroke="#82ca9d" activeDot={{ r: 8 }} />
-        </LineChart>
-      </ResponsiveContainer>
-    </div>
+  <div className="bg-gray-100 w-dvh h-dvh overflow-auto py-8 px-24 flex flex-wrap gap-8 justify-center items-start">
+    <LineChartCustom
+      title="Water consumption (m3)"
+      data={data.headers.map((header, idx) => ({
+        name: header,
+        waterConsumption: data.rows['Water consumption (m3)'][idx],
+      }))}
+    />
+    <LineChartCustom
+      title="Grid Electricity Consumption (KWh)"
+      data={data.headers.map((header, idx) => ({
+        name: header,
+        gridElectricityConsumption: data.rows['Grid Electricity Consumption (KWh)'][idx],
+      }))}
+    />
+    <LineChartCustom
+      title="Food waste (Kg)"
+      data={data.headers.map((header, idx) => ({
+        name: header,
+        foodWaste: data.rows['Food waste (Kg)'][idx],
+      }))}
+    />
+    <LineChartCustom
+      title="Combined"
+      data={data.headers.map((header, idx) => ({
+        name: header,
+        waterConsumption: data.rows['Water consumption (m3)'][idx],
+        gridElectricityConsumption: data.rows['Grid Electricity Consumption (KWh)'][idx],
+        foodWaste: data.rows['Food waste (Kg)'][idx],
+      }))}
+    />
   </div>
 );
 
