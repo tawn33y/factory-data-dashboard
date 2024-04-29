7e860268a3fa38bf87ad535f2923cb10dd92411a
Tony <tawn33y@gmail.com>
2024-04-29 03:44:36 -0400

    Update dropdown theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from '@vercel/analytics/react';
 import { PrimeReactProvider } from 'primereact/api';
-import "primereact/resources/themes/tailwind-light/theme.css";
+import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
